eac34c8f77aa2e931ef1f085a2ffcbfc65e8b6a6
tennyson-mccalla <tennyson.mccalla@gmail.com>
2017-08-16 15:21:46 -0400

    Added a default way of getting directory to search.

---
+++ Dimensions.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Script to get the dimensions of images in a directory and list them"""
+
+import sys
+# import os
+# from PIL import Image
+
+
+dir = sys.argv[1]
+
+
+def main():
+    """Where the real fun begins"""
+    pass
+
+
+def get_image_names_and_sizes(dir):
+    """Gets the filenames and image sizes as a 3-tuple, returns a list"""
+    pass
+
+
+def sorts(lis):
+    """Present the options for sorting (H, V, or A) then print to bash"""
+    pass
+
+
+if __name__ == '__main__':
+    sys.exit(main())

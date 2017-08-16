b804b58e1ac7ad9593f055fe41c51dc54959d3b7
tennyson-mccalla <tennyson.mccalla@gmail.com>
2017-08-16 17:24:44 -0400

    More filled out functions.

--- Dimensions.py
+++ Dimensions.py
@@ -4,26 +4,32 @@
 """Script to get the dimensions of images in a directory and list them"""
 
 import sys
-# import os
+# from os import path, listdir
 # from PIL import Image
 
 
-dir = sys.argv[1]
+folder = sys.argv[1]
 
 
 def main():
     """Where the real fun begins"""
-    pass
+    pics = get_image_names_and_sizes(folder)
+    print(sorts(pics))
 
 
 def get_image_names_and_sizes(dir):
     """Gets the filenames and image sizes as a 3-tuple, returns a list"""
-    pass
+    lis = []
+    for f in listdir(dir):
+        if '.DS_Store' in f:
+            continue
+        im = Image.open(dir + f)
+        lis.append(f, im.size, im.format)
 
 
 def sorts(lis):
-    """Present the options for sorting (H, V, or A) then print to bash"""
-    pass
+    """Present the options for sorting (H, W, or A) then print to bash"""
+    option = input("Sort by height (H), width (W), or area (A)?")
 
 
 if __name__ == '__main__':
